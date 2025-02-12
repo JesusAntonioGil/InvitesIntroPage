4999c5b5088cb3fa0a45251822f0543c1632b9c4
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-12 13:26:18 +0100

    Initial commit

---
+++ InvitesIntroPage/InvitesIntroPage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InvitesIntroPage
+//
+//  Created by Jesus Antonio Gil on 12/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InvitesIntroPage/InvitesIntroPage/InvitesIntroPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvitesIntroPageApp.swift
+//  InvitesIntroPage
+//
+//  Created by Jesus Antonio Gil on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct InvitesIntroPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
